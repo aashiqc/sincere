bf3af46b01b3ef41f01b6bb45661d379c649297a
Ashiq <muhmdashiqc@gmail.com>
2023-09-26 11:26:35 +0530

    updated

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", function() {
     // Set the interval for automatic sliding (1 second = 1000 milliseconds)
     const carousel = document.getElementById("carouselExampleCaptions");
     const myCarousel = new bootstrap.Carousel(carousel, {
-      interval: 1000
+      interval: 3000
     });
   });
 
   $(document).ready(function() {
     // Function to add the 'scrolled' class when scrolling down
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 50) { // Adjust the scroll threshold as needed
+        if ($(this).scrollTop() > 200) { // Adjust the scroll threshold as needed
             $('nav-container').addClass('is-sticky');
         } else {
             $('nav-container').removeClass('is-sticky');
